a5f9f079ddac5370d79e3ba6d63c10ee90a9577d
qqKdv <danil.14201@gmail.com>
2025-12-01 16:54:50 +0500

    removed the unnecessary

--- src/tests.py
+++ src/tests.py
@@ -13,57 +13,10 @@ class FlaskAppTests(unittest.TestCase):
         tested_app.app.config['TESTING'] = True
         self.app = tested_app.app.test_client()
 
-    def test_get_hello_endpoint(self):
-        r = self.app.get('/')
-        self.assertEqual(r.data, b'Hello World!')
-
-    def test_post_hello_endpoint(self):
-        r = self.app.post('/')
-        self.assertEqual(r.status_code, 405)
-
-    def test_get_api_endpoint(self):
-        r = self.app.get('/api')
-        self.assertEqual(r.json, {'status': 'test'})
-
-    def test_correct_post_api_endpoint(self):
-        r = self.app.post('/api',
-                          content_type='application/json',
-                          data=json.dumps({'name': 'Den', 'age': 100}))
-        self.assertEqual(r.json, {'status': 'OK'})
+    def test_multiply_success(self):
+        r = self.app.get('/multiply?a=5&b=2')
         self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.data, b'10.0')
 
-        r = self.app.post('/api',
-                          content_type='application/json',
-                          data=json.dumps({'name': 'Den'}))
-        self.assertEqual(r.json, {'status': 'OK'})
-        self.assertEqual(r.status_code, 200)
-
-    def test_not_dict_post_api_endpoint(self):
-        r = self.app.post('/api',
-                          content_type='application/json',
-                          data=json.dumps([{'name': 'Den'}]))
-        self.assertEqual(r.json, {'status': 'bad input'})
-        self.assertEqual(r.status_code, 400)
-
-    def test_no_name_post_api_endpoint(self):
-        r = self.app.post('/api',
-                          content_type='application/json',
-                          data=json.dumps({'age': 100}))
-        self.assertEqual(r.json, {'status': 'bad input'})
-        self.assertEqual(r.status_code, 400)
-
-    def test_bad_age_post_api_endpoint(self):
-        r = self.app.post('/api',
-                          content_type='application/json',
-                          data=json.dumps({'name': 'Den', 'age': '100'}))
-        self.assertEqual(r.json, {'status': 'bad input'})
-        self.assertEqual(r.status_code, 400)
-
-    def test_add_success(self):
-        r = self.app.get('/add?a=2&b=3')
-        self.assertEqual(r.status_code, 200)
-        self.assertEqual(r.data, b'5.0')
-##
-###
 if __name__ == '__main__':
     unittest.main()
